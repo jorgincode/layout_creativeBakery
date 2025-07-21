53c4ba641612163449ceef0b9b5a6523e285d488
Jorge Menezes Jr <jgrei.junior@gmail.com>
2025-07-21 19:17:17 -0300

    make build

---
+++ layout_creativeBakery.600ed65b.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){let e=document.getElementById("scrollToAbout"),t=document.getElementById("about-us");e&&t&&e.addEventListener("click",()=>{t.scrollIntoView({behavior:"smooth"})})});
+//# sourceMappingURL=layout_creativeBakery.600ed65b.js.map
